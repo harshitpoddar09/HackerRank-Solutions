8633b671f04e48528b8768afb096620c7503b43d
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-01-08 22:14:04 +0530

    Add files via upload

---
+++ Problem Solving/Bill Division.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    bill.pop(k)
+    if b==sum(bill)//2:
+        print('Bon Appetit')
+    else:
+        ans=b-(sum(bill)//2)
+        print(ans)
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
