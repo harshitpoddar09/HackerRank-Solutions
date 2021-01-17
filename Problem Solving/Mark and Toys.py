3694015e63b2ad8fd2654d6e4fcdab1109a9fe41
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-01-17 16:26:58 +0530

    Add files via upload

---
+++ Problem Solving/Mark and Toys.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    i=1
+    amount=prices[0]
+    k=k-amount
+    count=0
+    while k>=0:
+        count+=1
+        amount=prices[i]
+        k-=amount
+        i+=1
+    return count         
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
