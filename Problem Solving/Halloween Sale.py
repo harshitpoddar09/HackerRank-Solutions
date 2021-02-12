f2192106e30afeb8e1e232946066a5b0222a193d
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-12 20:37:12 +0530

    Add files via upload

---
+++ Problem Solving/Halloween Sale.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    count=0
+    cost=p
+    while s>=0:
+        if cost>m:
+            s=s-cost
+            count+=1
+            cost=cost-d
+        else:
+            cost=m
+            s=s-cost
+            count+=1
+    return count-1            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
