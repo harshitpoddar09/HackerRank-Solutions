4ae8e3da2a64c963fc2699eb866ceeb3427d965e
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-15 23:29:13 +0530

    Add files via upload

---
+++ Minimum Height Triangle.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import math
+
+def lowestTriangle(base, area):
+    # Complete this function
+    return math.ceil((2*area)/base)
+
+base, area = input().strip().split(' ')
+base, area = [int(base), int(area)]
+height = lowestTriangle(base, area)
+print(height)
