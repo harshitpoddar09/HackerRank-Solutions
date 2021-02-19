fc86a06075ad4c0f2dbabecfb182beedf64fba63
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-19 10:01:25 +0530

    Add files via upload

---
+++ Problem Solving/Counting Sort 1.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingSort function below.
+def countingSort(arr):
+    result = [0]*100
+    for i in range(len(arr)):
+        result[arr[i]]+=1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
