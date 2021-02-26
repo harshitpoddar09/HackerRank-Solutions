eba0aea84012a205484b73399dba2f90f84aa65a
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-26 12:46:44 +0530

    Add files via upload

---
+++ SQL/Higher Than 75 Marks.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT Name FROM STUDENTS  WHERE Marks>75 ORDER BY SUBSTR(Name,-3), ID;
