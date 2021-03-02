0db4a8528f9b48a8752750a25341d1d31c9b5a51
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-02 10:51:09 +0530

    Add files via upload

---
+++ SQL/The Blunder.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT CEIL(AVG(SALARY)-AVG(REPLACE(SALARY,'0',''))) FROM EMPLOYEES;
