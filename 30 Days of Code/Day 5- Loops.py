66dc055f4fc5b3b59b4c8ceeca785458dd40b074
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-09 22:03:27 +0530

    Add files via upload

---
+++ 30 Days of Code/Day 5- Loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+n = int(input())
+i=1
+while i<=10:
+    print ( str(n) + " x " + str(i) + " = "+ str(n*i))
+    i=i+1
+         
