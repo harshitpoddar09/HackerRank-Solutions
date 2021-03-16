1a9732ea0b48870cfc65090e7862bc85178a9887
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-16 13:05:01 +0530

    Add files via upload

---
+++ Problem Solving/Between Two Sets.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    # Write your code here
+    count=0
+    i=max(a)
+    while i<=min(b):
+        if all(i%j==0 for j in a) and all(k%i==0 for k in b):
+            count+=1
+        i+=1    
+    return count            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
