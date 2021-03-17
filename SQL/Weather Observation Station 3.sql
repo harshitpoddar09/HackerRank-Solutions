7f55ba2d5a48449b9e3100bed66d241bf1b1198d
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-17 08:21:07 +0530

    Update Weather Observation Station 3.sql

--- SQL/Weather Observation Station 3.sql
+++ SQL/Weather Observation Station 3.sql
@@ -1,5 +1,4 @@
 /*
 Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
