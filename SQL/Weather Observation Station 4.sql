24ca8284f77fd8fb89897c3f195de3dd9750c67e
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-18 10:30:31 +0530

    Update Weather Observation Station 4.sql

--- SQL/Weather Observation Station 4.sql
+++ SQL/Weather Observation Station 4.sql
@@ -1,5 +1,4 @@
 /*
 Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 SELECT (COUNT(CITY))-(COUNT(DISTINCT CITY)) FROM STATION;
