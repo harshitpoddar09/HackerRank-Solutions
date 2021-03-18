87c72b565a513f5fa0f4b730747ca7c61f7ebd14
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-18 10:31:29 +0530

    Update Higher Than 75 Marks.sql

--- SQL/Higher Than 75 Marks.sql
+++ SQL/Higher Than 75 Marks.sql
@@ -1,5 +1,4 @@
 /*
 Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 SELECT Name FROM STUDENTS  WHERE Marks>75 ORDER BY SUBSTR(Name,-3), ID;
