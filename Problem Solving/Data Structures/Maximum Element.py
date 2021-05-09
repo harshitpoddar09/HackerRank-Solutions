c825d675f6b34ff5b8fde0cc1be7c32a9e9ba900
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-05-09 12:53:28 +0530

    Add files via upload

---
+++ Problem Solving/Data Structures/Maximum Element.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getMax' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY operations as parameter.
+#
+
+def getMax(operations):
+    # Write your code here
+    stack=[0]
+    ans=[]
+    for i in operations:
+        if i=='3':
+            ans.append(stack[-1])
+        elif i=='2':
+            stack.pop()
+        else:
+            stack.append(max(int(i[2:]),stack[-1]))
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ops = []
+
+    for _ in range(n):
+        ops_item = input()
+        ops.append(ops_item)
+
+    res = getMax(ops)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
