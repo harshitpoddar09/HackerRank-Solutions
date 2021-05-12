c7b81264133a6f228462c8c6eb7e37a231c8ed9c
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-05-12 18:10:48 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Pangrams.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    s=s.lower()
+    alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in alphabets:
+        if i not in s:
+            return 'not pangram'
+    return 'pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
