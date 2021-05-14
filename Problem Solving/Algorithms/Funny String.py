60b6021694c5edf77c4c8267234a9b9279390184
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-05-14 11:29:32 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Funny String.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'funnyString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def funnyString(s):
+    # Write your code here
+    normal=[ord(i) for i in s]
+    diff=[abs(normal[j]-normal[j-1]) for j in range(1,len(normal))]
+    if diff==diff[::-1]:
+        return 'Funny'
+    else:
+        return 'Not Funny'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
