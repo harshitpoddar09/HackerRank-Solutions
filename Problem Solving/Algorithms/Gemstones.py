8eab081ccef61b99edff00842742bca6435d3cc7
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-05-22 12:06:42 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Gemstones.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    # Write your code here
+    ans=arr[0]
+    for i in ans:
+        for j in range(1,len(arr)):
+            if i not in arr[j]:
+                ans=ans.replace(i,'')
+                break
+    return len(set(ans))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
