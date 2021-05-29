13dbb97d170b7fd765381aea35a20cb9dc848cb5
Harshit Poddar <harshitpoddar09@gmail.com>
2021-05-29 12:56:57 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Sherlock and Array.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    left=0
+    right=sum(arr)
+    for i in range(len(arr)):
+        right-=arr[i]
+        if left==right:
+            return 'YES'
+        else:
+            left+=arr[i]
+    return 'NO'
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
