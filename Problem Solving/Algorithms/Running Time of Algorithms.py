dc13d83099c3c6fb795303005a572abddf5af3a7
Harshit Poddar <harshitpoddar09@gmail.com>
2021-06-02 22:39:23 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Running Time of Algorithms.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'runningTime' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def runningTime(arr):
+    # Write your code here
+    ans=0
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+            ans+=1
+        arr[j+1]=key
+    return ans 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
