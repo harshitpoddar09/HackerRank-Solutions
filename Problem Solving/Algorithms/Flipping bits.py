3b47bf697447e75f8d91b9f97c756172a8622bc6
Harshit Poddar <harshitpoddar09@gmail.com>
2021-06-03 20:23:04 +0530

    Add files via upload

---
+++ Problem Solving/Algorithms/Flipping bits.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    a=2**32         #(100000000000000000000000000000000)  ->   33 bits
+    a=a-1           #(11111111111111111111111111111111)   ->   32 bits
+    return n^a      #n xor a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
