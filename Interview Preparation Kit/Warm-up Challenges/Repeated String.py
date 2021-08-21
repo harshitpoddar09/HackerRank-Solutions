4f9b976815db23f12a4c34a05b5b2fcd8d2b44a4
Harshit Poddar <harshitpoddar09@gmail.com>
2021-08-21 15:20:54 +0530

    Add files via upload

---
+++ Interview Preparation Kit/Warm-up Challenges/Repeated String.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    count=0
+    for i in s:
+        if i=='a':
+            count+=1
+    ans=count*(n//len(s))
+    for j in range(n%len(s)):
+        if s[j]=='a':
+            ans+=1
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
