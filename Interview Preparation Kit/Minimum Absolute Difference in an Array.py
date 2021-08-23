128cd8025a22fa87a5c23a969f38aa08af0ddd57
Harshit Poddar <harshitpoddar09@gmail.com>
2021-08-23 16:37:07 +0530

    Add files via upload

---
+++ Interview Preparation Kit/Minimum Absolute Difference in an Array.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    minimum=abs(arr[0]-arr[1])
+    for i in range(len(arr)-1):
+        diff=abs(arr[i]-arr[i+1])
+        if diff<minimum:
+            minimum=diff
+    return minimum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
