026eaedcdcab21b3e4fbf1214a176779491f27b3
Harshit Poddar <harshitpoddar09@gmail.com>
2021-08-24 18:02:52 +0530

    Add files via upload

---
+++ Interview Preparation Kit/Sorting/Sorting- Bubble Sort.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    swaps=0
+    for i in range(len(a)):
+        for j in range(n-i-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                swaps+=1
+    print('Array is sorted in '+str(swaps)+' swaps.')
+    print('First Element: '+str(a[0]))
+    print('Last Element: '+str(a[-1]))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
